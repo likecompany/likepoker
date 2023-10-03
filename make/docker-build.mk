d0036edd56d57d017467ffb71bed384c7dc8e26c
toymaj <sermed512@gmail.com>
2023-10-04 02:04:05 +0300

    refactor makefile

--- make/docker-build.mk
+++ make/docker-build.mk
@@ -1,22 +1,11 @@
-docker = docker
-compose = compose
-
-web = $(docker)/$(compose)/web.yml
-networks = $(docker)/networks.yml
-
-docker_v2 = docker compose
-
-env = .env
-names = .names
-
 ifdef name
 	ifdef run
-		containers = $(docker_v2) $(foreach container,$(run),-f $(name)/$(docker)/$(compose)/$(container).yml) -f $(networks) --env-file $(env)
+		containers = $(foreach container,$(run),-f $(name)/$(shell cat $(name)/.docker)/$(container).yml) -f $(name)/$(shell cat $(name)/.docker-networks) --env-file .env
 
 		ifdef captures
 			containers += --abort-on-container-exit --exit-code-from $(captures)
 		endif
 	endif
 else
-	containers = $(docker_v2) -f $(web) $(foreach name,$(shell cat $(names)),-f $(name)/$(docker)/$(compose)/app.yml -f $(name)/$(docker)/$(compose)/db.yml) -f $(networks) --env-file $(env)
+	containers = $(foreach name,$(shell cat .names),$(foreach compose,$(shell cat $(name)/.docker-rule),-f $(name)/$(compose))) --env-file .env
 endif
--- make/docker.mk
+++ make/docker.mk
@@ -1,8 +1,10 @@
 include make/check-defined.mk
 include make/docker-build.mk
 
+docker = docker compose
+
 .PHONY: start
 start:
 	$(check_defined, command)
 
-	$(containers) $(command)
+	$(docker) $(containers) $(command)
